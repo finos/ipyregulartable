e98e7c96e57929fa5a9df6a243e4137848c11aa8
Tim Paine <t.paine154@gmail.com>
2020-10-07 14:23:55 -0400

    fixes code for #11

--- ipyregulartable/datamodel/base.py
+++ ipyregulartable/datamodel/base.py
@@ -1,3 +1,10 @@
+# *****************************************************************************
+#
+# Copyright (c) 2020, the ipyregulartable authors.
+#
+# This file is part of the jupyterlab_templates library, distributed under the terms of
+# the Apache License 2.0.  The full license can be found in the LICENSE file.
+#
 from abc import ABCMeta, abstractmethod
 from six import with_metaclass
 
